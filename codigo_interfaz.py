2f78e7bc1a941e263696f6de7a74b3dee474f031
li-heracles <li.heracles@duocuc.cl>
2026-04-14 12:17:34 -0400

    Update codigo_interfaz.py

--- codigo_interfaz.py
+++ codigo_interfaz.py
@@ -2,6 +2,10 @@
 from tkinter import *
 from tkinter import ttk
 
+#Para importar custom tkinter
+import customtkinter
+
+root = tk()
 # Creación de un cuadro
 frame = tkk.frame(parent)
 
@@ -31,11 +35,34 @@ resultsContents.set('Valor inicial para mostrar')
 current = resultsContents.get()
 resultsContents.set('nuevo valor a mostrar')
 
+#agregar imagen 
+image = PhotoImage(file='Myimage.png')
+label['image'] = image
+
+#
+label['font'] = "TKDefaultFont"
+
 # entrada (ingresar texto)
 nombre_usuario = stringvar()
 nombre = ttk.entry(parent, textvariable=nombre_usuario)
 
+# Agregar numero de caracteres a la entrada 
+nombre['width'] = 30
+
+# agregar contraseña
+contrase = ttk.entry(parent, textvariable=password, show="*")
+
+#colores a usar 
+#fire brick
+#agua
+#turquesa
+
+#barra de movimiento
+barmov = ttk.scrollbar( parent, orient=vertical, comand=listbox.yview)
+listbox.configure(yscrollcommand= set)
 
 
-
-
+examenvar = Stringvar()
+examen =ttk.Combox(parent, textvariable=examenvar)
+examen['values'] = ['Traumatología','Obesidad y Nutrición','Ginecologia y Obtetricia')
+examen.state(["readonly")]
